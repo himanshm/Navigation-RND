e0e14f12e467fc80a3876cfa910dd95a3cd2329d
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-04-26 15:11:21 +0530

    Add first navigation using react-navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator(); // Stack is an object with two properties where every property holds an object that acts as a component. Therefore, we can use those nested component objects.
 
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
-      <CategoriesScreen />
+      <StatusBar style='auto' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
