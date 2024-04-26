3466163cf8656cb131435304d6735140188e6f64
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-04-26 14:52:56 +0530

    Display items in a grid

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <>
+      <StatusBar style='light' />
       <CategoriesScreen />
     </>
   );
--- components/CategoryGridTile.tsx
+++ components/CategoryGridTile.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, Text, View, StyleSheet, Platform } from 'react-native';
 
 type CategoryGridTileProps = {
   title: string;
@@ -7,10 +7,16 @@ type CategoryGridTileProps = {
 
 function CategoryGridTile({ title, color }: CategoryGridTileProps) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: '#CCC' }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -18,3 +24,34 @@ function CategoryGridTile({ title, color }: CategoryGridTileProps) {
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+  },
+  button: { flex: 1 },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
